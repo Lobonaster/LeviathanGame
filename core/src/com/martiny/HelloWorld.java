81901ffdb8e993781f54c44b96846b98268a7827
Lobonaster <artem_lebedev_2004@mail.ru>
2024-05-01 16:25:34 +0300

    git test

--- core/src/com/martiny/HelloWorld.java
+++ core/src/com/martiny/HelloWorld.java
@@ -1,37 +1,18 @@
 package com.martiny;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.GLTexture;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class HelloWorld extends Game {
-
-	SpriteBatch batch;
-	Texture img;
-	Texture img2;
-	
+	public SpriteBatch batch;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("assets/Pictures/badlogic.jpg");
-		img2 = new Texture("assets/Pictures/verg.jpg");
+		this.setScreen(new GameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.draw(img2, 300,200);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.render();
 	}
 }
