8798e52250259063b6e9d6bdafc466c123aab69a
Lobonaster <artem_lebedev_2004@mail.ru>
2024-05-12 22:20:41 +0300

    Great Settings

--- core/src/com/lebedev/Settings_Screen.java
+++ core/src/com/lebedev/Settings_Screen.java
@@ -2,37 +2,131 @@ package com.lebedev;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 public class Settings_Screen implements Screen {
-    // TODO: 12.05.2024 needs a full remake
     LeviathanGame game;
-    private static final int EXIT_W = 100;
-    private static final int EXIT_H = 100;
-    private static final int EXIT_Y = LeviathanGame.HEIGHT / 2  - 100 / 2;
-    private static final int SETTINGS_W = 700;
-    private static final int SETTINGS_H = 100;
-    private static final int SETTINGS_Y = LeviathanGame.HEIGHT / 2 + 200 / 2  ;
+    private Stage stage;
     private Skin skin;
-    private CheckBox fullscreenCheckbox;
-    private SelectBox<String> resolutionSelectBox;
-    Texture exit_button;
-    Texture exit_button_hover;
-    Texture settings_button;
-    Texture settings_button_hover;
-    Button play_btn;
+    private Texture menuBG;
+    private ExtendViewport extendViewport;
+    private int res_width = 1280; // chosen resolution width
+    private int res_height = 720; // chosen resolution height
 
-
-    public Settings_Screen (LeviathanGame game) {
+    public Settings_Screen(LeviathanGame game){
         this.game = game;
-        exit_button_hover = new Texture("assets/Pictures/Buttons/exit_hover.png");
-        exit_button = new Texture("assets/Pictures/Buttons/exit.png");
-        settings_button = new Texture("assets/Pictures/Buttons/settings.png");
-        settings_button_hover = new Texture("assets/Pictures/Buttons/settings_hover.png");
+        extendViewport = new ExtendViewport(res_width, res_height); // For background
+
+        game.batch = new SpriteBatch();
+        menuBG = new Texture("assets/Pictures/Menus/SettingsMenu.png");
+
+        stage = new Stage(new ExtendViewport(res_width, res_height)); // For UI
+        Gdx.input.setInputProcessor(stage);
+
+        Table root = new Table();
+        root.setFillParent(true);
+        stage.addActor(root);
+
+        skin = new Skin(Gdx.files.internal("assets/skin2/uiskin.json")); //TODO: Temporary change to basic uiskin
+
+        Table menuButtons = new Table();
+
+        final SelectBox selectBox = new SelectBox(skin);
+
+        selectBox.setAlignment(Align.right);
+        selectBox.getList().setAlignment(Align.right);
+        selectBox.getStyle().listStyle.selection.setRightWidth(10);
+        selectBox.getStyle().listStyle.selection.setLeftWidth(20);
+
+        selectBox.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                System.out.println(selectBox.getSelected());
+            }
+        });
+        selectBox.setItems("1280x720", "1920x1080", "2560x1440", "3840x2160");
+        selectBox.setSelected("1280x720");
+        selectBox.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                // Process of converting a selected string into 2 int variables
+                String select = selectBox.getSelection().toString();
+                select = select.replaceAll("[{}]", "");
+                int index = select.indexOf("x");
+                String str1 = select.substring(0, index);
+                String str2 = select.substring(index + 1);
+                res_width = Integer.parseInt(str1); // chosen resolution
+                res_height = Integer.parseInt(str2); // chosen resolution
+            }
+        });
+
+        root.add(menuButtons).expandY().expandX().left();
+
+        menuButtons.defaults().padLeft(80).spaceTop(20).width(450).height(100);
+
+        menuButtons.row();
+        Button toggle_button = new Button(skin,"toggle");
+        menuButtons.add(toggle_button).padTop(222).colspan(2);
+        Label label = new Label("FULLSCREEN",skin);
+        toggle_button.add(label).expandX().left().padLeft(40);
+
+        menuButtons.row();
+
+        TextButton apply_button = new TextButton("APPLY",skin);
+        menuButtons.add(apply_button).width(180);
+        apply_button.getLabel().setAlignment(Align.left);
+        apply_button.getLabelCell().padLeft(40);
+
+        menuButtons.add(selectBox).width(250).height(100).padLeft(20);
+
+        menuButtons.row();
+        TextButton return_button = new TextButton("RETURN",skin);
+        menuButtons.add(return_button).colspan(2);
+        return_button.getLabel().setAlignment(Align.left);
+        return_button.getLabelCell().padLeft(40);
+
+        toggle_button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                if (toggle_button.isChecked()) {
+                    System.out.println("FullScreen");
+                    Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+                    // Prohibition to change resolution while fullscreen
+                    apply_button.setDisabled(true);
+                    selectBox.setDisabled(true);
+                } else {
+                    System.out.println("Windowed");
+                    Gdx.graphics.setWindowedMode(res_width,res_height);
+                    // Allowance to change resolution while not fullscreen
+                    apply_button.setDisabled(false);
+                    selectBox.setDisabled(false);
+                }
+            }
+        });
+        apply_button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println(res_width +"x"+ res_height);
+                Gdx.graphics.setWindowedMode(res_width,res_height);
+            }
+        });
+        return_button.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println("RETURN");
+                game.setScreen(new MainMenuScreen(game));
+            }
+        });
+
+        stage.setDebugAll(true);
     }
 
     @Override
@@ -42,13 +136,22 @@ public class Settings_Screen implements Screen {
 
     @Override
     public void render(float delta) {
-
+        ScreenUtils.clear(new Color(0x0f140bf7));
+        extendViewport.apply();
+        game.batch.setProjectionMatrix(extendViewport.getCamera().combined);
+        game.batch.begin();
+        game.batch.draw(menuBG,-640,-360,1280,720);
+        game.batch.end();
+        stage.act();
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
+        extendViewport.update(width,height);
     }
+
     @Override
     public void pause() {
 
@@ -66,104 +169,7 @@ public class Settings_Screen implements Screen {
 
     @Override
     public void dispose() {
-
+        game.batch.dispose();
+        stage.dispose();
     }
 }
-/**
-public class Settings_Screen implements Screen {
-    private final LeviathanGame game;
-    private Skin skin;
-    private CheckBox fullscreenCheckbox;
-    private SelectBox<String> resolutionSelectBox;
-
-    public Settings_Screen(LeviathanGame game) {
-        this.game = game;
-    }
-
-    @Override
-    public void show() {
-        skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
-
-        fullscreenCheckbox = new CheckBox("Fullscreen", skin);
-        fullscreenCheckbox.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                game.setFullscreen(((CheckBox) actor).isChecked());
-            }
-        });
-
-        String[] resolutions = {"1400x720", "1920x1080"};
-        resolutionSelectBox = new SelectBox<>(skin);
-        resolutionSelectBox.setItems(resolutions);
-        resolutionSelectBox.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                SelectBox<String> selectBox = (SelectBox<String>) actor;
-                String resolution = selectBox.getSelected();
-                if (resolution.equals("1400x720")) {
-                    game.setResolution(1400, 720);
-                } else if (resolution.equals("1920x1080")) {
-                    game.setResolution(1920, 1080);
-                }
-            }
-        });
-
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-                if (fullscreenCheckbox.isVisible()) {
-                    fullscreenCheckbox.setVisible(false);
-                    resolutionSelectBox.setVisible(true);
-                } else {
-                    fullscreenCheckbox.setVisible(true);
-                    resolutionSelectBox.setVisible(false);
-                }
-                return true;
-            }
-        });
-
-        fullscreenCheckbox.setPosition(10, 10);
-        fullscreenCheckbox.setVisible(true);
-        resolutionSelectBox.setPosition(10, 50);
-        resolutionSelectBox.setVisible(false);
-    }
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        game.batch.begin();
-
-        fullscreenCheckbox.draw(game.batch, 1);
-        resolutionSelectBox.draw(game.batch, 1);
-
-        game.batch.end();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        float aspectRatio = (float) width / height;
-        game.camera.viewportWidth = 1080 * aspectRatio;
-        game.camera.viewportHeight = 1080;
-        game.camera.position.set(game.camera.viewportWidth / 2, game.camera.viewportHeight / 2, 0);
-        game.camera.update();
-    }
-
-    @Override
-    public void pause() {
-    }
-
-    @Override
-    public void resume() {
-    }
-
-    @Override
-    public void hide() {
-        skin.dispose();
-    }
-
-    @Override
-    public void dispose() {
-    }
-} */
