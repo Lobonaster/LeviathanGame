9e4961e7e14584e43ec7c1d74b883e1833bd02de
Lobonaster <artem_lebedev_2004@mail.ru>
2024-05-14 16:09:49 +0300

    Updated to Actor extender

--- core/src/com/lebedev/VergVisuals.java
+++ core/src/com/lebedev/VergVisuals.java
@@ -1,40 +1,47 @@
 package com.lebedev;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public class VergVisuals {
-    // TODO: 12.05.2024 needs a full remake
+public class VergVisuals extends Actor {
+    // TODO: 14.05.2024 still needs polishing
     public static final int VERG_WIDTH_PIXEL = 17;
     public static final int VERG_HEIGHT_PIXEL = 32;
     public static final int VERG_WIDTH = VERG_WIDTH_PIXEL * 7;
     public static final int VERG_HEIGHT = VERG_HEIGHT_PIXEL * 7;
     public static final float VERG_ANIMATION_SPEED = 0.5f;
-    public static float verg_x, verg_y;
-    public static Animation[] rolls;
-    public static float rollTimer;
-    public static int roll;
+    public static float verg_x  = 200;
+    private static float verg_y = 450;
+    public static float rollTimer = 0 ;
+    public static int roll = 2;
+    private float stateTime = 0;
+    static TextureRegion[][] rollSpriteSheet = TextureRegion.split(
+            new Texture("assets/Pictures/Sprites/DEMO.png"),
+            VERG_WIDTH_PIXEL, VERG_HEIGHT_PIXEL);
+    public static Animation[] rolls = new Animation[5];
 
-    public Animation[] init() {
-
-        verg_y = -200;
-        verg_x = -450;
-        rollTimer = 0;
-        roll = 2;
-
-        rolls = new Animation[5];
-
-        TextureRegion[][] rollSpriteSheet = TextureRegion.split(
-                new Texture("assets/Pictures/Sprites/DEMO.png"),
-                VERG_WIDTH_PIXEL, VERG_HEIGHT_PIXEL);
-
+    static {
         rolls[0] = new Animation(VERG_ANIMATION_SPEED, rollSpriteSheet[2]); // right (hit)
         rolls[1] = new Animation(VERG_ANIMATION_SPEED, rollSpriteSheet[1]); // (attack)
-        rolls[2] = new Animation(VERG_ANIMATION_SPEED, rollSpriteSheet[0]); // mid (idle)
+        rolls[2] = new Animation(VERG_ANIMATION_SPEED, rollSpriteSheet[0]);  // mid (idle)
         rolls[3] = new Animation(VERG_ANIMATION_SPEED, rollSpriteSheet[3]); //(defend)
         rolls[4] = new Animation(VERG_ANIMATION_SPEED, rollSpriteSheet[4]); // left (ego)
-
-        return rolls;
     }
+    public void setPos(int x, int y){
+        verg_x = x;
+        verg_y = y;
+    }
+    public void getDelta(float delta){
+        stateTime += delta;
+    }
+    @Override
+    public void draw(Batch batch, float parentAlpha){
+        batch.draw((TextureRegion) rolls[roll].getKeyFrame(stateTime, true), verg_x, verg_y, VERG_WIDTH, VERG_HEIGHT);
+    }
+
 }
